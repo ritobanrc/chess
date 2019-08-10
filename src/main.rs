b2165946a37fee9e199b99e3973831039b2582d2
Ritoban Roy-Chowdhury <ritobanrc@gmail.com>
2019-08-10 13:56:00 -0700

    conrod doesn't work. reddit post

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 
+#[macro_use] extern crate conrod_core;
+extern crate conrod_piston;
+
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::*;
 use piston::event_loop::*;
@@ -23,6 +26,12 @@ pub const BOARD_BORDER_SIZE: f64 = 5.0;
 pub const WIDTH: f64 = 600.0;
 pub const HEIGHT: f64 = 450.0 + 2.0*BOARD_BORDER_SIZE;
 
+widget_ids! {
+    pub struct Ids {
+        canvas,
+        title
+    }
+}
 
 fn main() {
     // Change this to OpenGL::V2_1 if not working.
@@ -36,6 +45,9 @@ fn main() {
         .build()
         .unwrap();
 
+    let mut ui = conrod_core::UiBuilder::new([WIDTH, HEIGHT]).build();
+    let image_map = conrod_core::image::Map::<opengl_graphics::Texture>::new();
+    let ids = Ids::new(ui.widget_id_generator());
 
     let chessboard = Chessboard::standard();
     let view_settings = { ChessboardViewSettings::new() };
@@ -53,9 +65,30 @@ fn main() {
                 use graphics::clear;
                 clear([0.0; 4], gl);
                 view.draw(&controller, &c, gl);
-
+                if let Some(primitives) = ui.draw_if_changed() {
+                    conrod_piston::draw::primitives(primitives,
+                                                        c,
+                                                        gl,
+                                                        /*what*/);
+                }
             });
         }
+
+        if let Some(e) = conrod_piston::event::convert(e.clone(), WIDTH, HEIGHT) {
+            ui.handle_event(e);
+        }
+
+        e.update(|_| {
+            use conrod_core::{widget, Colorable, Labelable, Positionable, Sizeable, Widget};
+            let mut ui = ui.set_widgets();
+
+            const TITLE: &'static str = "All Widgets";
+            const MARGIN: conrod_core::Scalar = 30.0;
+            const TITLE_SIZE: conrod_core::FontSize = 42;
+            widget::Canvas::new().pad(MARGIN).scroll_kids_vertically().set(ids.canvas, &mut ui);
+            widget::Text::new(TITLE).font_size(TITLE_SIZE).mid_top_of(ids.canvas).set(ids.title, &mut ui);
+        });
+
         controller.event(&e);
     }
 }
